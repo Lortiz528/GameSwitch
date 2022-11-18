2f27eed219b99aa8bc212dca40dbbb5de6b93a8c
lily08 <liliangelxd@gmail.com>
2022-11-18 13:05:09 -0500

    user-queries

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,6 +1,6 @@
 //dependencies
 const express = require("express");
-const { getAllUsers } = require("../queries/users");
+const { getAllUsers, getUserByEmail, updateUser} = require("../queries/users");
 
 //sub routes
 const usersController = express.Router();
@@ -15,5 +15,17 @@ usersController.get("/", async (req, res) => {
     res.status(404).json({ sucess: false, message: "no users" });
   }
 });
+
+// usersController.get('/:id', async (req, res) => {
+//   const { id } = req.params
+//   const addUser = await updateUser(id)
+//   if (addUser[0]) {
+//     res.status(200).json({ success: true, payload: users[0] })
+//   } else {
+//     res.status(404).json({ success: false, payload: 'not found' })
+//   }
+// })
+
+
 
 module.exports = usersController;
--- backend/queries/users.js
+++ backend/queries/users.js
@@ -1,16 +1,78 @@
 //import the db object
-const db = require("../db/dbConfig.js");
+const db = require('../db/dbConfig.js')
 
 //get all users info from the database
 const getAllUsers = async () => {
   try {
-    const allUsers = await db.any("SELECT * FROM users");
-    return allUsers;
+    const allUsers = await db.any('SELECT * FROM users')
+    return allUsers
   } catch (error) {
-    res.status(404).json({ sucess: false, message: "no users" });
+    res.status(404).json({ sucess: false, message: 'no users' })
   }
-};
+}
+
+const getUserByEmail = async (user_email) => {
+  try {
+    const user = await db.any(
+      'SELECT * FROM users where user_email=$1',
+      user_email
+    )
+    return user
+  } catch (error) {
+    res.status(404).json({ sucess: false, message: 'no user' })
+  }
+}
+
+// const updateUser = async (user, user_email) => {
+//   const { user_name, user_trade_score, user_location, user_avatar } = user
+//   try {
+//     const user = await db.one(
+//       'update users set user_name=$1, user_trade_score=$2, user_location=$3,user_avatar=$4 where user_email=$1 returning *',
+//       [user_name, user_trade_score, user_location, user_avatar]
+//     )
+//     console.log(user)
+//     return user
+//   } catch (error) {
+//     res.status(404).json({ sucess: false, message: 'no user' })
+//   }
+// }
+
+// const createUser = async (user) => {
+//   const {user_name,user_email,user_password,user_trade_score,user_location,user_avatar} = user
+//   try {
+//     const addUser = await db.one(
+//       'insert into users (user_name,user_email,user_password,user_trade_score,user_location,user_avatar) values ($1,$2,$3,$4,$5,$6) returning *',
+//       [
+//         user_name,
+//         user_email,
+//         user_password,
+//         user_trade_score,
+//         user_location,
+//         user_avatar,
+//       ]
+//     )
+//     return addUser
+//   } catch (error) {
+//     res.status(404).json({ sucess: false, message: 'no user' })
+//   }
+// }
+
+const deleteUser = async (user_email) => {
+  try {
+    const oneUser = await db.one(
+      'DELETE FROM users WHERE user_email=$1 RETURNING *',
+      user_email
+    )
+    return oneUser
+  } catch (error) {
+    console.log(error.message || error)
+    return error
+  }
+}
 
 module.exports = {
   getAllUsers,
-};
+  getUserByEmail,
+  updateUser,
+  deleteUser,
+}
