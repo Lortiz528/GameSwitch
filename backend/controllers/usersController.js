921de064d3f0a194b528c827e8180e4c1f55b61f
lily08 <liliangelxd@gmail.com>
2022-11-19 11:43:09 -0500

    user-controller

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,31 +1,72 @@
 //dependencies
-const express = require("express");
-const { getAllUsers, getUserByEmail, updateUser} = require("../queries/users");
+const express = require('express')
+const {
+  getAllUsers,
+  getUserByEmail,
+  updateUser,
+  deleteUser,
+  createUser,
+} = require('../queries/users')
 
 //sub routes
-const usersController = express.Router();
+const usersController = express.Router()
 
 //index route
 //display all users
-usersController.get("/", async (req, res) => {
+usersController.get('/', async (req, res) => {
   try {
-    const allUsers = await getAllUsers();
-    res.status(200).json({ success: true, payload: allUsers });
+    const allUsers = await getAllUsers()
+    res.status(200).json({ success: true, payload: allUsers })
   } catch (error) {
-    res.status(404).json({ sucess: false, message: "no users" });
-  }
-});
-
-usersController.get('/:email', async (req, res) => {
-  const { email } = req.params
-  const addUser = await getUserByEmail(email)
-  if (addUser[0]) {
-    res.status(200).json({ success: true, payload: addUser[0] })
-  } else {
-    res.status(404).json({ success: false, payload: 'not found' })
+    res.status(404).json({ sucess: false, message: 'no users' })
   }
 })
 
+//get one user
+usersController.get('/:email', async (req, res) => {
+  const { email } = req.params
+  const getUser = await getUserByEmail(email)
+  if (getUser[0]) {
+    res.status(200).json({ success: true, payload: getUser[0] })
+  } else {
+    res.status(404).json({ success: false, payload: `no user found with email of ${email}` })
+  }
+})
 
+//update user
+usersController.put('/:email', async (req, res) => {
+  const { email } = req.params
+  const changeUser = await updateUser(req.body,email)
 
-module.exports = usersController;
+  if (changeUser.user_email) {
+    res.status(200).json({ payload: changeUser })
+  } else {
+    res.status(404).json('update error')
+  }
+
+})
+
+//create user
+usersController.post('/newuser', async (req, res) => {
+  const addUser = await createUser(req.body)
+
+  if (addUser) {
+    res.status(200).json({ success: true, payload: addUser })
+  } else {
+    res.status(404).send({ success: false, payload: 'create error' })
+  }
+})
+
+//delete user
+usersController.delete('/:email', async (req, res) => {
+  const { email } = req.params
+  const removeUser = await deleteUser(email)
+  
+  if (removeUser.user_email) {
+    res.status(200).json({ success: true, payload: removeUser })
+  } else {
+    res.status(404).json({ success: false, payload: email })
+  }
+})
+
+module.exports = usersController
--- backend/queries/users.js
+++ backend/queries/users.js
@@ -23,39 +23,32 @@ const getUserByEmail = async (user_email) => {
   }
 }
 
-// const updateUser = async (user, user_email) => {
-//   const { user_name, user_trade_score, user_location, user_avatar } = user
-//   try {
-//     const user = await db.one(
-//       'update users set user_name=$1, user_trade_score=$2, user_location=$3,user_avatar=$4 where user_email=$1 returning *',
-//       [user_name, user_trade_score, user_location, user_avatar]
-//     )
-//     console.log(user)
-//     return user
-//   } catch (error) {
-//     res.status(404).json({ sucess: false, message: 'no user' })
-//   }
-// }
+const updateUser = async (user, email) => {
+  const { user_name, user_trade_score, user_location, user_avatar } = user
+  try {
+    const user = await db.one(
+      'update users set user_name=$1, user_trade_score=$2, user_location=$3,user_avatar=$4 where user_email=$5 returning *',
+      [user_name, user_trade_score, user_location, user_avatar, email]
+    )
+    return user
+  } catch (error) {
+    res.status(404).json({ sucess: false, message: 'no user' })
+  }
+}
 
-// const createUser = async (user) => {
-//   const {user_name,user_email,user_password,user_trade_score,user_location,user_avatar} = user
-//   try {
-//     const addUser = await db.one(
-//       'insert into users (user_name,user_email,user_password,user_trade_score,user_location,user_avatar) values ($1,$2,$3,$4,$5,$6) returning *',
-//       [
-//         user_name,
-//         user_email,
-//         user_password,
-//         user_trade_score,
-//         user_location,
-//         user_avatar,
-//       ]
-//     )
-//     return addUser
-//   } catch (error) {
-//     res.status(404).json({ sucess: false, message: 'no user' })
-//   }
-// }
+const createUser = async (user) => {
+  const { user_name, user_email, user_password } = user
+
+  try {
+    const addUser = await db.one(
+      'insert into users (user_name,user_email,user_password) values ($1,$2,$3) returning *',
+      [user_name, user_email, user_password]
+    )
+    return addUser
+  } catch (error) {
+    res.status(404).json({ sucess: false, message: 'no user' })
+  }
+}
 
 const deleteUser = async (user_email) => {
   try {
@@ -69,10 +62,10 @@ const deleteUser = async (user_email) => {
     return error
   }
 }
-
 module.exports = {
   getAllUsers,
   getUserByEmail,
-  // updateUser,
+  createUser,
+  updateUser,
   deleteUser,
 }
