4852f77926e543919c70eb03bb090a9ecd83cc4f
JuHaoChen1997 <juhaochen97@gmail.com>
2022-11-19 20:12:15 -0500

    add firebase test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import './App.css'
+import "./App.css";
 // import Edit from './Pages/Edit'
 // import FourOFour from './Pages/FourOFour'
-import Home from './pages/Home'
-import UsersIndex from './pages/UsersIndex'
+import Home from "./pages/Home";
+import UsersIndex from "./pages/UsersIndex";
 // import New from './Pages/New'
 // import Show from './Pages/Show'
-import About from './pages/About'
-import { Routes, Route } from 'react-router-dom'
-import NavBar from './components/NavBar'
-
-
+import About from "./pages/About";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Signup from "./firebaseTest/Signup";
 
 function App() {
   return (
-    <div className='App'>
-      <NavBar/>
+    <div className="App">
+      <NavBar />
       <main>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/users' element={<UsersIndex />} />
-          <Route path='/about' element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UsersIndex />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
