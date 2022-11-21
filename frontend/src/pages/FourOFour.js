6cd8580fb0ac514c2a9bcdfab21e9ec85ed157a5
PratimaRoy <pratimaroy170@gmail.com>
2022-11-21 11:46:49 -0500

    Added the fourofour in pages folder

---
+++ frontend/src/pages/FourOFour.js
@@ -0,0 +1,13 @@
+
+function FourOFour() {
+  return <div className="fourfour">
+    <h1>Error!! No page found!</h1>
+    <Button>
+      <Link to='/' >Go Home</Link>
+    </Button>
+
+    
+    </div>;
+}
+
+export default FourOFour;
