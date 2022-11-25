7698fb262d7332cd6ad4e27a291514aa469882ac
lily08 <liliangelxd@gmail.com>
2022-11-25 15:31:55 -0500

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Home from './pages/Home'
 import UsersIndex from './pages/UsersIndex'
 // import New from './Pages/New'
 // import Show from './Pages/Show'
-<<<<<<< HEAD
 import About from './pages/About'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
@@ -18,20 +17,7 @@ import { CurrentUserContext } from './components/CurrentUserContext'
 import { useEffect, useState } from 'react'
 import GameSwitchDescription from './pages/GameSwitchDescription'
 import GameDetail from './components/GameDetail'
-=======
-import About from "./pages/About";
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Signup from "./firebaseTest/Signup";
-import Login from "./firebaseTest/Login";
-import UserProfile from "./components/UserProfile";
-import ResetPassWord from "./firebaseTest/ResetPassword";
-import UserProfileUpdate from "./components/UserProfileUpdate";
-import { CurrentUserContext } from "./components/CurrentUserContext";
-import { useEffect, useState } from "react";
-import GameSwitchDescription from "./pages/GameSwitchDescription";
-import GameCollection from "./gamecollection/GameCollection";
->>>>>>> 2a8260211213b4d6b155c2e9ed8228d28b8522ba
+import GameCollection from './gamecollection/GameCollection'
 function App() {
   //stores the user info throughout the whole app
   const [currentUser, setCurrentUser] = useState({})
@@ -42,40 +28,24 @@ function App() {
         <NavBar />
         <main>
           <Routes>
-<<<<<<< HEAD
             <Route path='/' element={<Home />} />
             <Route path='/users' element={<UsersIndex />} />
+            <Route path='/users/:user_email' element={<User />} />
             <Route path='/about' element={<About />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/login' element={<Login />} />
             <Route path='/userprofile' element={<UserProfile />} />
             <Route path='/resetpassword' element={<ResetPassWord />} />
             <Route path='/updateprofile' element={<UserProfileUpdate />} />
-            <Route path='/tutorial' element={<GameSwitchDescription />} />
             <Route path='/games/:gameId' element={<GameDetail />} />
+            <Route path='/gamecollection' element={<GameCollection />} />
+            <Route path='/tutorial' element={<GameSwitchDescription />} />
             <Route path='*' element={<FourOFour />} />
-=======
-            <Route path="/" element={<Home />} />
-            <Route path="/users" element={<UsersIndex />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/userprofile" element={<UserProfile />} />
-            <Route path="/resetpassword" element={<ResetPassWord />} />
-            <Route path="/updateprofile" element={<UserProfileUpdate />} />
-            <Route path="/gamecollection" element={<GameCollection />} />
-            <Route path="/tutorial" element={<GameSwitchDescription />} />
-            <Route path="*" element={<FourOFour />} />
->>>>>>> 2a8260211213b4d6b155c2e9ed8228d28b8522ba
           </Routes>
         </main>
       </CurrentUserContext.Provider>
     </div>
-<<<<<<< HEAD
   )
-=======
-  );
->>>>>>> 2a8260211213b4d6b155c2e9ed8228d28b8522ba
 }
 
 export default App
