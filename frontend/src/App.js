a122142db6062f942006ef88f18d1a2f71af4805
JuHaoChen1997 <juhaochen97@gmail.com>
2022-11-26 19:13:42 -0500

    add new game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ import { CurrentUserContext } from "./components/CurrentUserContext";
 import { useEffect, useState } from "react";
 import GameSwitchDescription from "./pages/GameSwitchDescription";
 import GameCollection from "./gamecollection/GameCollection";
+import AddNewGame from "./gamecollection/AddNewGame";
+
 function App() {
   //stores the user info throughout the whole app
   const [currentUser, setCurrentUser] = useState({});
@@ -36,6 +38,7 @@ function App() {
             <Route path="/resetpassword" element={<ResetPassWord />} />
             <Route path="/updateprofile" element={<UserProfileUpdate />} />
             <Route path="/gamecollection" element={<GameCollection />} />
+            <Route path="/addnewgame" element={<AddNewGame />} />
             <Route path="/tutorial" element={<GameSwitchDescription />} />
             <Route path="*" element={<FourOFour />} />
           </Routes>
