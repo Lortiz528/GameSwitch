2898ae2a97fdcce1e844cdce732039fdcb9b696d
Lortiz528 <lortiztech@gmail.com>
2022-12-06 11:46:26 -0500

    fixed logic game detail page

--- frontend/src/components/GameDetail.js
+++ frontend/src/components/GameDetail.js
@@ -1,77 +1,77 @@
-import axios from 'axios'
-import { useEffect, useState, useContext } from 'react'
-import { CurrentUserContext } from './CurrentUserContext'
-import { useNavigate, useParams, Link } from 'react-router-dom'
-import TradeRequest from './TradeRequest'
-import { Container, Row, Col } from 'react-bootstrap'
-import './GameDetail.css'
-const API = process.env.REACT_APP_API_URL
+import axios from 'axios';
+import { useEffect, useState, useContext } from 'react';
+import { CurrentUserContext } from './CurrentUserContext';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import TradeRequest from './TradeRequest';
+import { Container, Row, Col } from 'react-bootstrap';
+import './GameDetail.css';
+const API = process.env.REACT_APP_API_URL;
 
 function GameDetail() {
-  const { gameId } = useParams()
-  const { currentUser } = useContext(CurrentUserContext)
-  const [game, setGame] = useState([])
-  const [user, setUser] = useState([])
-  const [show, setShow] = useState(false)
-  let navigate = useNavigate()
+  const { gameId } = useParams();
+  const { currentUser } = useContext(CurrentUserContext);
+  const [game, setGame] = useState([]);
+  const [user, setUser] = useState([]);
+  const [show, setShow] = useState(false);
+  let navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get(`${API}/games/${gameId}`)
       .then((res) => {
-        setGame(res.data.payload)
+        setGame(res.data.payload);
       })
       .catch(() => {
-        navigate('/not-found')
-      })
-  }, [gameId, navigate])
+        navigate('/not-found');
+      });
+  }, [gameId, navigate]);
 
   useEffect(() => {
     axios
       .get(`${API}/users`)
       .then((res) => {
-        setUser(res.data.payload)
+        setUser(res.data.payload);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+        console.log(err);
+      });
+  }, []);
 
   const findUserName = (users) => {
     for (let user of users) {
       if (user.user_id === game.user_id) {
-        return user.user_name
+        return user.user_name;
       }
     }
-  }
+  };
 
   const findUserEmail = (users) => {
     for (let user of users) {
       if (user.user_id === game.user_id) {
-        return user.user_email
+        return user.user_email;
       }
     }
-  }
+  };
 
-  const handleOpen = () => setShow(true)
-  const handleClose = () => setShow(false)
+  const handleOpen = () => setShow(true);
+  const handleClose = () => setShow(false);
 
   return (
     <div>
-      <Container fluid className='mt-4'>
+      <Container fluid className="mt-4">
         <Row>
           <Col>
             <img
-              className='game-img'
+              className="game-img"
               src={game.game_img}
-              alt='game pic'
+              alt="game pic"
               height={500}
             />
           </Col>
           <Col>
-            <div className='game-detail'>
+            <div className="game-detail">
               <h2>{game.game_name}</h2>
-              <p className='description'>
+              <p className="description">
                 <strong>Description:</strong> {game.game_description}
               </p>
               <p>
@@ -89,9 +89,10 @@ function GameDetail() {
               {currentUser.user_name !== findUserName(user) &&
               currentUser.user_name ? (
                 <button onClick={handleOpen}>offer trade</button>
-              ) : (
-                <Link to='/signup'>Sign Up or Login In</Link>
-              )}
+              ) : null}
+              {!currentUser.user_name ? (
+                <Link to="/signup">Sign up or Log in to offer trade</Link>
+              ) : null}
               <TradeRequest
                 handleClose={handleClose}
                 show={show}
@@ -104,6 +105,6 @@ function GameDetail() {
         </Row>
       </Container>
     </div>
-  )
+  );
 }
-export default GameDetail
+export default GameDetail;
