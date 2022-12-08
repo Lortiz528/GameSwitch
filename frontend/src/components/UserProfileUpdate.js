19f98fa6e679c6488c63d47e1bf5d772d6d3e116
Lortiz528 <lortiztech@gmail.com>
2022-12-08 13:58:48 -0500

    fixed query bug users

--- frontend/src/components/UserProfileUpdate.js
+++ frontend/src/components/UserProfileUpdate.js
@@ -1,189 +1,190 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { CurrentUserContext } from '../components/CurrentUserContext'
-import { useContext } from 'react'
-import { Form, Image } from 'react-bootstrap'
-import './UserProfileUpdate.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { CurrentUserContext } from '../components/CurrentUserContext';
+import { useContext } from 'react';
+import { Form, Image } from 'react-bootstrap';
+import './UserProfileUpdate.css';
 
 //API url
-const API = process.env.REACT_APP_API_URL
+const API = process.env.REACT_APP_API_URL;
 
 function UserProfileUpdate() {
-  const { currentUser } = useContext(CurrentUserContext)
+  const { currentUser } = useContext(CurrentUserContext);
   //console.log(currentUser);
 
   const [userInput, setUserInput] = useState({
     user_name: '',
-    user_trade_score: '',
     user_location: '',
     user_avatar: '',
     user_bio: '',
-  })
+    user_facebook: '',
+    user_instagram: '',
+    user_twitch: '',
+  });
 
   useEffect(() => {
     axios
       .get(`${API}/users/${currentUser.user_email}`)
       .then((res) => {
-        setUserInput(res.data.payload)
+        setUserInput(res.data.payload);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }, [currentUser.user_email])
-  const navigate = useNavigate()
+        console.log(error);
+      });
+  }, [currentUser.user_email]);
+  const navigate = useNavigate();
 
   const updateProfile = () => {
     axios
       .put(`${API}/users/${currentUser.user_email}`, userInput)
       .then(() => {
-        currentUser.user_name = userInput.user_name
-        currentUser.user_location = userInput.user_location
-        currentUser.user_avatar = userInput.user_avatar
-        currentUser.user_bio = userInput.user_bio
-        navigate('/userprofile')
+        currentUser.user_name = userInput.user_name;
+        currentUser.user_location = userInput.user_location;
+        currentUser.user_avatar = userInput.user_avatar;
+        currentUser.user_bio = userInput.user_bio;
+        currentUser.user_facebook = userInput.user_facebook;
+        currentUser.user_instagram = userInput.user_instagram;
+        currentUser.user_twitch = userInput.user_twitch;
+        navigate('/userprofile');
       })
-      .catch((error) => console.error('catch', error))
-  }
+      .catch((error) => console.error('catch', error));
+  };
 
   const handleTextChange = (event) => {
-    setUserInput({ ...userInput, [event.target.id]: event.target.value })
-  }
+    setUserInput({ ...userInput, [event.target.id]: event.target.value });
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    updateProfile()
+    event.preventDefault();
+    updateProfile();
     setUserInput({
       user_password: '',
       user_confirmPassWord: '',
-    })
-  }
+    });
+  };
 
   //  [user_name, user_trade_score, user_location, user_avatar, email]
 
   return (
-    <section className='userProfileUpdateForm'>
+    <section className="userProfileUpdateForm">
       <h2>Update Profile </h2>
-      <Form onSubmit={handleSubmit} className='form'>
-        <Form.Group className='mb-3'>
+      <Form onSubmit={handleSubmit} className="form">
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>UserName:</strong>{' '}
           </Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter Username'
-            id='user_name'
+            type="text"
+            placeholder="Enter Username"
+            id="user_name"
             value={userInput.user_name}
             onChange={handleTextChange}
             required
           />
         </Form.Group>
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>Location:</strong>{' '}
           </Form.Label>
           <Form.Select
-            type='text'
-            id='user_location'
+            type="text"
+            id="user_location"
             value={userInput.user_location}
             onChange={handleTextChange}
             required
           >
-            <option value='Manhattan'>Manhattan</option>
-            <option value='Queens'>Queens</option>
-            <option value='Staten Island'>Staten Island</option>
-            <option value='Bronx'>Bronx</option>
-            <option value='Brooklyn'>Brooklyn</option>
+            <option value="Manhattan">Manhattan</option>
+            <option value="Queens">Queens</option>
+            <option value="Staten Island">Staten Island</option>
+            <option value="Bronx">Bronx</option>
+            <option value="Brooklyn">Brooklyn</option>
           </Form.Select>
         </Form.Group>
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>Bio:</strong>{' '}
           </Form.Label>
           <Form.Control
-            type='text'
-            placeholder='User Bio'
-            id='user_bio'
+            type="text"
+            placeholder="User Bio"
+            id="user_bio"
             value={userInput.user_bio}
             onChange={handleTextChange}
             required
           />
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>Facebook:</strong>
           </Form.Label>
           <Form.Control
-            type='text'
-            placeholder='facebook Url'
-            id='user_facebook'
+            type="text"
+            placeholder="facebook Url"
+            id="user_facebook"
             value={userInput.user_facebook}
             onChange={handleTextChange}
-            required
           />
         </Form.Group>
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>Instagram:</strong>{' '}
           </Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Instagram Url'
-            id='user_instagram'
+            type="text"
+            placeholder="Instagram Url"
+            id="user_instagram"
             value={userInput.user_instagram}
             onChange={handleTextChange}
-            required
           />
         </Form.Group>
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>Twitch:</strong>{' '}
           </Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Twitch Url'
-            id='user_Twitch'
+            type="text"
+            placeholder="Twitch Url"
+            id="user_Twitch"
             value={userInput.user_twitch}
             onChange={handleTextChange}
-            required
           />
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group className="mb-3">
           <Form.Label>
             <strong>Avatar:</strong>{' '}
           </Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter Avatar Url'
-            id='user_avatar'
+            type="text"
+            placeholder="Enter Avatar Url"
+            id="user_avatar"
             value={userInput.user_avatar}
             onChange={handleTextChange}
             required
           />
         </Form.Group>
-        <Image
-          roundedCircle
-          src={userInput.user_avatar}
-          alt={userInput.user_name}
-        />
+        <Image src={userInput.user_avatar} alt={userInput.user_name} />
         <br></br>
         <br />
-        <button className='update-profile' type='submit'>
+        {/* <button className='update-profile' type='submit'>
           Update Profile
-        </button>
+        </button> */}
+        <input
+          className="update-profile"
+          type="submit"
+          value="update profile"
+        />
       </Form>
       <br />
 
-      <Link to='/userprofile'>
-        <button className='cancel-profile'>Cancel </button>
-      </Link>
+      <Link to="/userprofile">Go back</Link>
 
       <br></br>
     </section>
-  )
+  );
 }
 
-export default UserProfileUpdate
+export default UserProfileUpdate;
