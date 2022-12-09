9e143a63059653984083b049503a8fa7314cd431
JuHaoChen1997 <juhaochen97@gmail.com>
2022-12-09 10:33:37 -0500

    add buttons update

--- frontend/src/tradeRequestRecords/ReceivedRecord.js
+++ frontend/src/tradeRequestRecords/ReceivedRecord.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import SignOut from '../firebaseTest/Signout';
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { setLogLevel } from 'firebase/app';
-import { Card, Row, Container, Button } from 'react-bootstrap';
+import React from "react";
+import SignOut from "../firebaseTest/Signout";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { setLogLevel } from "firebase/app";
+import { Card, Row, Container, Button } from "react-bootstrap";
 
 const API = process.env.REACT_APP_API_URL; //localhost:3333
 
@@ -18,8 +18,8 @@ export default function ReceivedRecord({ receivedRequest }) {
       receivedRequest.trade_complete_from_offerer;
     acceptRequest.trade_complete_from_receiver =
       receivedRequest.trade_complete_from_receiver;
-    acceptRequest.trade_success = 'accepted';
-    receivedRequest.trade_success = 'accepted';
+    acceptRequest.trade_success = "accepted";
+    receivedRequest.trade_success = "accepted";
     acceptRequest.trade_id = receivedRequest.trade_id;
 
     //console.log("accepted request", acceptRequest);
@@ -27,7 +27,7 @@ export default function ReceivedRecord({ receivedRequest }) {
     axios
       .put(`${API}/trades/updatetrade`, acceptRequest)
       .then((res) => {
-        setStatus('accepted');
+        setStatus("accepted");
       })
       .catch((error) => console.log(error));
   };
@@ -38,14 +38,14 @@ export default function ReceivedRecord({ receivedRequest }) {
       receivedRequest.trade_complete_from_offerer;
     rejectRequest.trade_complete_from_receiver =
       receivedRequest.trade_complete_from_receiver;
-    rejectRequest.trade_success = 'rejected';
-    receivedRequest.trade_success = 'rejected';
+    rejectRequest.trade_success = "rejected";
+    receivedRequest.trade_success = "rejected";
     rejectRequest.trade_id = receivedRequest.trade_id;
 
     axios
       .put(`${API}/trades/updatetrade`, rejectRequest)
       .then((res) => {
-        setStatus('rejected');
+        setStatus("rejected");
       })
       .catch((error) => console.log(error));
   };
@@ -54,7 +54,7 @@ export default function ReceivedRecord({ receivedRequest }) {
     receivedRequest.trade_complete_from_receiver = true;
 
     if (receivedRequest.trade_complete_from_offerer === true) {
-      receivedRequest.trade_success = 'Completed';
+      receivedRequest.trade_success = "Completed";
       //swap games here
       const gamesInfo = {};
       gamesInfo.offerer_id = receivedRequest.trade_offerer_user_id;
@@ -65,7 +65,7 @@ export default function ReceivedRecord({ receivedRequest }) {
         .put(`${API}/trades/swapgames`, gamesInfo)
         .then((res) => {
           // setRequest(res.data.payload);
-          setStatus('Completed');
+          setStatus("Completed");
         })
         .catch((error) => console.log(error));
     }
@@ -79,7 +79,7 @@ export default function ReceivedRecord({ receivedRequest }) {
   };
 
   const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
@@ -88,31 +88,44 @@ export default function ReceivedRecord({ receivedRequest }) {
   //console.log("request is", receivedRequest);
 
   return (
-    <Card style={{ width: '20rem', textAlign: 'left' }}>
+    <Card style={{ width: "20rem", textAlign: "left" }}>
       <Card.Body>
         <Card.Title>Trade Offer Date: {formatDate(dateString)}</Card.Title>
         <h5>Trade Status: {status}</h5>
         <Card.Title>
           {receivedRequest.offer_name} Complete Status:
-          {request.trade_complete_from_offerer ? 'True' : 'false'}
+          {request.trade_complete_from_offerer ? "True" : "false"}
         </Card.Title>
         <Card.Title>
           {receivedRequest.receiver_name} Complete Status:
-          {request.trade_complete_from_receiver ? 'True' : 'false'}
+          {request.trade_complete_from_receiver ? "True" : "false"}
         </Card.Title>
         <Card.Text>
           {`${receivedRequest.offer_name} Offered ${receivedRequest.offerer_game_name} for ${receivedRequest.receiver_name}'s Copy of ${receivedRequest.receiver_game_name}`}
         </Card.Text>
 
-        <Button variant="primary" onClick={accept}>Accept</Button>
+        {receivedRequest.trade_success === "completed" ? null : (
+          <Button variant="primary" onClick={accept}>
+            Accept
+          </Button>
+        )}
         <br></br>
         <br></br>
 
-        <Button variant="warning" onClick={reject}>Reject</Button>
+        {receivedRequest.trade_success === "completed" ? null : (
+          <Button variant="warning" onClick={reject}>
+            Reject
+          </Button>
+        )}
         <br></br>
         <br></br>
 
-        <Button variant="success" onClick={completeTrade}>Confirm Complete Trade</Button>
+        {receivedRequest.trade_success === "pending" ||
+        receivedRequest.trade_success === "rejected" ? null : (
+          <Button variant="success" onClick={completeTrade}>
+            Confirm Complete Trade
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
