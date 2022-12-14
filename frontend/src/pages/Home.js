c97adf80378db4425161eb3e328df33c302a129e
Lortiz528 <lortiztech@gmail.com>
2022-12-14 17:13:59 -0500

    typo home screen

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,10 +3,10 @@ import './Home.css'
 function Home() {
   return (
     <div className='home'>
-      <h1 className='home-title'>Welcome Game Switch NYC !</h1>
+      <h1 className='home-title'>Welcome To Game Switch NYC!</h1>
       <br></br>
       {/* <span class='start-btn'>START</span> */}
-      <h2>Sign up or Log in to get started!</h2>
+      <h2>Sign up or Log in to get started</h2>
 
       {/* <img width="400" src="https://media2.giphy.com/media/fAD9SMlNWp0k84Ra1G/giphy.gif?cid=790b7611d3fec240996c139a98fa99ad753b3ea083818e0e&rid=giphy.gif&ct=g"></img>   */}
       <img src='https://cdn.dribbble.com/users/989157/screenshots/3825479/comp-4_1.gif' />
